0509003d888417eaad35644ce8d3a47394f4bd54
VikitorXD <rm551886@fiap.com.br>
2023-11-13 23:48:28 -0300

    cabecalho finalizado

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -28,4 +28,28 @@ export default function Cabecalho() {
       </div>
       </header>
     );
+  } else {
+    return (
+      <header className="bg-white text-azulPorto shadow-lg text-center shadow-custom">
+        <div className="mx-auto max-w-2xl flex items-center justify-center flex-col">
+        <Image src="/portoLogo.png" alt="Logo da Porto Seguro" width={100} height={90}/>
+        <hr className="border-azulPorto border-t-2 w-80 mt-1"/>
+        <nav className="menu">
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/login">Sobre a Solução</Link>
+            </li>
+          </ul>
+        </nav>
+        </div>
+      <button className="absolute top-0 right-0 mt-2 mr-2 bg-azulPorto-600 text-black px-4 py-2 rounded">
+        <Image src="/user.png" alt="Icone de usuário" width={32} height={30}></Image>
+        <Link href="/login"></Link>
+      </button>
+      </header>
+    );
+  }
 }
