7c426070cd7ba34af4f6514f9a3a7898b631141b
VikitorXD <rm551886@fiap.com.br>
2023-11-13 23:49:35 -0300

    usePages em andametno

--- src/app/user-pages/page.jsx
+++ src/app/user-pages/page.jsx
@@ -4,5 +4,19 @@ import { useEffect, useState } from "react";
 
 export default function UserPages() {
 
+    const [users, setUsers] = useState([]);
 
+        useEffect(() => {
+            fetch("http://localhost:3000/api/git-users")
+            .then((response)=> response.json())
+            .then((response)=> setUsers(response))
+            .catch(errors=> console.error(errors));
+
+        }, [])
+        
+        console.log(users);
+
+  return (
+    
+  )
 }
